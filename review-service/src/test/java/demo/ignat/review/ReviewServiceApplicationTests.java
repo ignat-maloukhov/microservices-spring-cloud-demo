b998615618e9ab6810a564fb7b46edefdddec9de
Ignat Maloukhov <cityofdrawbridges@gmail.com>
2023-08-25 18:43:14 +0300

    Init commit

---
+++ review-service/src/test/java/demo/ignat/review/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ignat.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
