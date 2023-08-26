90a1e7fb6b0288570a6ddd8d2bbc56e606b29bc6
Ignat Maloukhov <cityofdrawbridges@gmail.com>
2023-08-26 16:12:29 +0300

    Add product-service implementation

---
+++ microservices/product-service/src/main/java/demo/ignat/microservices/core/product/services/DefaultProductService.java
@@ -0,0 +1,39 @@
+package demo.ignat.microservices.core.product.services;
+
+import demo.ignat.api.core.product.Product;
+import demo.ignat.api.core.product.ProductService;
+import demo.ignat.api.exception.InvalidInputException;
+import demo.ignat.api.exception.NotFoundException;
+import demo.ignat.util.http.ServiceUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultProductService implements ProductService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultProductService.class);
+
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public DefaultProductService(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+    @Override
+    public Product getProduct(int id) {
+        LOG.debug("/product return the found product for productId={}", id);
+
+        if (id < 1) {
+            throw new InvalidInputException("Invalid productId: " + id);
+        }
+
+        if (id == 13) {
+            throw new NotFoundException("No product found for productId: " + id);
+        }
+
+        return new Product(id, "name-" + id, 123, serviceUtil.getServiceAddress());
+    }
+}
---
+++ microservices/product-service/src/test/java/demo/ignat/microservices/core/product/ProductServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package demo.ignat.microservices.core.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
